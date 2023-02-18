e9b4a1fb30b7856686f818d40198ff8a64077f20
Kwizera-bonheur123 <kbonheur123@gmail.com>
2023-02-18 12:32:45 +0200

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**
+ *  * main - prints alpha in lower case and upper case
+ *   * Return: Always 0
+ *    */
+int main(void)
+{
+char c;
+c = 'a';
+while(c <= 'z'){
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
