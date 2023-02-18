d3f45b6b8de002930d64fbc72594a8159e17a72e
Kwizera-bonheur123 <kbonheur123@gmail.com>
2023-02-18 10:37:46 +0200

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,21 +9,22 @@
 int main(void)
 {
 
-int n;
+	int n;
 
-srand(time(0))
-n = rand() - RAND_MAX	/ 2;
-if (n > 0)
-{
-printf("%d id positive\n", n);
+	srand(time(0))
+		n = rand() - RAND_MAX   / 2;
+	if (n > 0)
+	{
+		printf("%d id positive\n", n);
+	}
+	else if (n == o)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
-else if (n == o)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
-return (0);
-}
+
