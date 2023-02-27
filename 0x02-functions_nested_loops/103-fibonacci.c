2e5318c328b8c6f361f756baa8e8a55036b39db8
Kwizera-bonheur123 <kbonheur123@gmail.com>
2023-02-27 17:16:39 +0200

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * main - prints the sum of Even fibonacci numbers 
+ * main - prints the sum of Even fibonacci numbers
  * less than 4000000
  * Return: nothing!
  */
-
 int main(void)
 {
 int i = 0;
 long j = 1, k = 2, sum = k;
-
 while (k + j < 4000000)
 {
 k += j;
