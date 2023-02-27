bb5efbe751611672d441f2b443e63e10f303961f
Kwizera-bonheur123 <kbonheur123@gmail.com>
2023-02-27 11:45:09 +0200

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * _isalpha - Check with alphabet character
- * @c: the character to be checked 
+ * @c: the character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
-
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
